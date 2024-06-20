48ba36aa7a9669ed9a89db851d775663ed6023ca
vannguyenv12 <npnv.vn1@gmail.com>
2024-06-20 20:31:33 +0700

    schema validation 1

--- src/features/user/controller/user.controller.ts
+++ src/features/user/controller/user.controller.ts
@@ -1,12 +1,20 @@
 import { User } from "@prisma/client";
 import { Request, Response } from "express";
 import { prisma } from "~/prisma";
+import { userSchemaCreate } from "../schema/user.schema";
 
 class UserController {
    public async createUser(req: Request, res: Response) {
     const {
       email, password, firstName, lastName, avatar
     } = req.body;
+
+    const {error} = userSchemaCreate.validate(req.body);
+
+    if (error) {
+      console.log(error);
+      return;
+    }
 
     // Insert To DB
     const newUser: User = await prisma.user.create({
