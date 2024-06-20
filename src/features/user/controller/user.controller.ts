58fbc77dd9b84c66ed7567a339d5387ac94385b9
vannguyenv12 <npnv.vn1@gmail.com>
2024-06-20 20:04:06 +0700

    setup user controller

---
+++ src/features/user/controller/user.controller.ts
@@ -0,0 +1,23 @@
+import { User } from "@prisma/client";
+import { Request, Response } from "express";
+import { prisma } from "~/prisma";
+
+class UserController {
+   public async createUser(req: Request, res: Response) {
+    const {
+      email, password, firstName, lastName, avatar
+    } = req.body;
+
+    // Insert To DB
+    const newUser: User = await prisma.user.create({
+      data: {
+        email, password, firstName, lastName, avatar
+      }
+    });
+
+    res.status(201).json(newUser); // CREATED
+
+  }
+}
+
+export const userController: UserController = new UserController();
