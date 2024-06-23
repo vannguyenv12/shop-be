bea6cc71924b7b40be10885dbaaaa7549689f962
vannguyenv12 <npnv.vn1@gmail.com>
2024-06-23 14:52:24 +0700

    create address table

---
+++ prisma/migrations/20240623075040_add_address_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(191) NOT NULL,
+    `province` VARCHAR(191) NULL,
+    `country` VARCHAR(191) NOT NULL,
+    `postalCode` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Address` ADD CONSTRAINT `Address_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
